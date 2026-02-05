d0a1d86ec574c71a98069d2bf1087f344813b1d6
binhchungcongbinh <binhchungcongbinh1946@gmail.com>
2026-02-05 17:42:34 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 /* --- 1. DANH SÁCH ẢNH (Bạn thay tên file thực tế vào đây) --- */
 const imageList = [
-    '1.jpg','2.png', '3.jpg', '4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg', '12.png', '13.jpg', '14.jpg','15.jpg','16.jpg','17.jpg','18.jpg','19.jpg','20.jpg','21.jpg', '22.jpg','23.jpg',
-    '24.jpg','25.jpg','26.jpg','27.jpg','28.jpg',  'anh1.jpg', 'anh2.jpg', 'anh3.jpg', 'anh4.jpg', 'anh5.jpg',  'anh6.jpg', 'anh7.jpg', 'anh8.jpg',
+    '1.jpg','2.jpg', '3.jpg', '4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg', '12.jpg', '13.jpg', '14.jpg','15.jpg','16.jpg','17.jpg','18.jpg','19.jpg','20.jpg','21.jpg', '22.jpg','23.jpg',
+    '24.jpg','25.jpg','26.jpg','27.jpg','28.jpg',  'anh1.jpg', 'anh2.jpg', 'anh3.jpg', 'anh4.png', 'anh5.jpg',  'anh6.jpg', 'anh7.jpg', 'anh8.jpg',
 ];
 
 let currentIndex = 0; // Chỉ số ảnh đang hiển thị
